0a9ed12c17dcb94aa3d800408ea8ec852299ac49
chilonghai <chilonghai@thsolar.com>
2017-06-21 10:09:58 +0800

    添加main方法, demo

--- collect-connection/src/main/java/space/tilkai/Application.java
+++ collect-connection/src/main/java/space/tilkai/Application.java
@@ -1,8 +1,11 @@
 package space.tilkai;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- * @author tilkai
+ * Created by tilkai on 2017/6/21.
  */
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
